ac5389afb4babaa5609156cafe47ab457df90faf
Ragib Gulzar <ragibgulzar@gmail.com>
2026-08-10 00:59:08 +0530

    Add Halloumi Boxes [A] from Codeforces

---
+++ 1903/A - Halloumi Boxes/solution.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+ 
+public class HalloumiBoxes{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+ 
+        while(t-- > 0){
+ 
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++){
+                arr[i] = sc.nextInt();
+            }
+ 
+            if(k>1){
+                System.out.println("YES");
+                continue;
+            }
+            boolean isSorted = true;
+            for(int i = 1; i < n; i++){
+                if(arr[i]<arr[i-1]){
+                    isSorted = false;
+                    break;
+                }
+            }
+            System.out.println(isSorted ? "YES" : "NO");
+        }
+ 
+    }
+}
